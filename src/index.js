74f9f536e1df958a169b179ae96c36f03133be33
Gabe Nielsen <gabenielsen7@gmail.com>
2022-04-22 08:58:00 -0700

    setup index.js to make api call and display results

--- src/index.js
+++ src/index.js
@@ -3,3 +3,12 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import CurrencyExchange from "./js/currency";
+
+async function apiCall() {
+  const response = await CurrencyExchange.getExchangeRates();
+  displayResults(response);
+}
+
+function displayResults() {}
+
+$("#exchange").on("click", function () {});
