f6177211bc9c483a699e16d8194b3b432f22bfe6
Gabe Nielsen <gabenielsen7@gmail.com>
2022-04-22 11:56:01 -0700

    correct error in AUD and round exchange number to .00

--- src/js/exchange.js
+++ src/js/exchange.js
@@ -1,5 +1,5 @@
 export function exchange(response, usdValue, currency) {
   let exchangeRate = response.conversion_rates[`${currency}`];
   let conversion = usdValue * exchangeRate;
-  return conversion;
+  return conversion.toFixed(2);
 }
