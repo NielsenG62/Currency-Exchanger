38c481a9186e13bc14d0506e61ad84d11ce17b9c
Gabe Nielsen <gabenielsen7@gmail.com>
2022-04-22 09:56:29 -0700

    WIP: testing the displayResults formula

--- src/index.js
+++ src/index.js
@@ -4,11 +4,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import CurrencyExchange from "./js/currency";
 
-async function apiCall() {
+async function apiCall(currency) {
   const response = await CurrencyExchange.getExchangeRates();
-  displayResults(response);
+  displayResults(response, currency);
 }
 
-function displayResults() {}
+function displayResults(response, currency) {
+  if (response.result === "success") {
+    let usdValue = $("#usd-value").val();
+    let conversionRate = response.conversion_rates.currency;
+    $("#usd").text = usdValue;
+    $("#exchange").text = `${usdValue * conversionRate}`;
+  }
+}
 
-$("#exchange").on("click", function () {});
+$("#submit").on("click", function () {
+  let currency = $("#exchange-select").val();
+  apiCall();
+});
