e159b7a7d4c9afc515c79f2be5e4b6520c988b5b
Gabe Nielsen <gabenielsen7@gmail.com>
2022-04-22 13:26:07 -0700

    replace broken api link with working one

--- src/js/currency.js
+++ src/js/currency.js
@@ -2,7 +2,7 @@ export default class CurrencyExchange {
   static async getExchangeRates() {
     try {
       const response = await fetch(
-        `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/KPW`
+        `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`
       );
       if (!response.ok) {
         throw Error(response.status);
