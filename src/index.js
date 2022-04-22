391003244e10a24645e7d8ddd687ae4d81a14827
Gabe Nielsen <gabenielsen7@gmail.com>
2022-04-22 15:30:14 -0700

    add styling

--- src/index.js
+++ src/index.js
@@ -15,9 +15,11 @@ function displayResults(response, currency, usdValue) {
     $("#usd").text(usdValue);
     let conversion = exchange(response, usdValue, currency);
     if (conversion === false) {
+      $("h2").addClass("hidden");
       $("#error").html(
         `It appears this currency code doesn't exist. Please look at the <a href="https://www.exchangerate-api.com/docs/supported-currencies">supported currency codes</a>`
       );
+      $("#error").removeClass("hidden");
       return;
     }
     $("#exchange").text(conversion + " " + currency);
@@ -29,11 +31,13 @@ function displayResults(response, currency, usdValue) {
 
 $("form").on("submit", function (event) {
   event.preventDefault();
-  $("#error").html("");
+  $("#error").addClass("hidden");
+  $("#error").html("placeholder");
   let currency = $("#exchange-select").val();
   let usdValue = $("#usd-value").val();
   if (currency === "null" || usdValue === "") {
     $("#error").text("Please fill in the two fields");
+    $("#error").removeClass("hidden");
     return;
   }
   apiCall(currency, usdValue);
