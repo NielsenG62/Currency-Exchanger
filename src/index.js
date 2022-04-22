dcb8b7538969a00fbbb3fc0b2dd088586e3e695d
Gabe Nielsen <gabenielsen7@gmail.com>
2022-04-22 10:19:45 -0700

    WIP: still cannot get the exchange rate from api

--- src/index.js
+++ src/index.js
@@ -4,21 +4,27 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import CurrencyExchange from "./js/currency";
 
-async function apiCall(currency) {
+async function apiCall() {
   const response = await CurrencyExchange.getExchangeRates();
-  displayResults(response, currency);
+  displayResults(response);
 }
 
-function displayResults(response, currency) {
+function exchange(response, usdValue) {
+  let exchangeRate = response.conversion_rates.currency;
+  let conversion = usdValue * exchangeRate;
+  return conversion;
+}
+
+function displayResults(response) {
   if (response.result === "success") {
     let usdValue = $("#usd-value").val();
-    let conversionRate = response.conversion_rates.currency;
-    $("#usd").text = usdValue;
-    $("#exchange").text = `${usdValue * conversionRate}`;
+    let currency = $("#exchange-select").val();
+    $("#usd").text(usdValue);
+    let conversion = exchange(response, usdValue);
+    $("#exchange").text(conversion + " " + currency);
   }
 }
 
 $("#submit").on("click", function () {
-  let currency = $("#exchange-select").val();
   apiCall();
 });
